4cd6e71bc5297c63b929c5a15713e5c141e5c1ce
supuna <92195424+supuna28@users.noreply.github.com>
2022-05-18 19:12:06 +0530

    Update update.js

--- plugins/update.js
+++ plugins/update.js
@@ -1,7 +1,7 @@
 let { execSync } = require('child_process')
 let handler = async (m, { conn, text, isROwner }) => {
   if (global.conn.user.jid == conn.user.jid) {
-    let stdout = execSync('git remote set-url origin https://github.com/ilmanhdyt/ShiraoriBOT.git && git pull' + (isROwner && text ? ' ' + text : ''))
+    let stdout = execSync('git pull https://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@github.com/supuna28/BOTKINGDOMVVIP2.git && git pull' + (isROwner && text ? ' ' + text : ''))
     if (isROwner) require('fs').readdirSync('plugins').map(v => global.reload('', v))
     m.reply(stdout.toString())
   }
